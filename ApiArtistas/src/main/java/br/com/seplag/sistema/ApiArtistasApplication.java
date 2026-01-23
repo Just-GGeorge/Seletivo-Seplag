16edc4a81455018912d3e79c67402addaf0b0e47
Guilherme George Oliveira da Silva <guilhermesilva@seplag.mt.gov.br>
2026-01-23 16:27:21 -0400

    Inicialização Docker
    Subindo o projeto inicial do Backend, postgreSQL e o MinIO

---
+++ ApiArtistas/src/main/java/br/com/seplag/sistema/ApiArtistasApplication.java
@@ -0,0 +1,13 @@
+package br.com.seplag.sistema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiArtistasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiArtistasApplication.class, args);
+	}
+
+}
